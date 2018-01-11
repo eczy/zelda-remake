9327159a156e7c7f076a8757569c673e6806dd74
Evan Czyzycki <eczy3826@gmail.com>
2018-01-11 17:41:58 -0500

    3577: up to zelda_t6

---
+++ Assets/Scripts/Collector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collector : MonoBehaviour {
+
+	Inventory inventory;
+
+	void Start ()
+	{
+		// Try to grab a reference to the inventory component on this gameobject.
+		inventory = GetComponent<Inventory> ();
+		if (inventory == null)
+			Debug.LogWarning ("WARNING: Gameobject with a collector has no inventory!");
+	}
+
+	void OnTriggerEnter (Collider coll)
+	{
+		GameObject object_collided_with = coll.gameObject;
+
+		if (object_collided_with.tag == "rupee")
+		{
+			// Check to see if inventory exists before adding rupee to it.
+			if (inventory != null)
+				inventory.AddRupees (1);
+			Destroy (object_collided_with);
+		}
+	}
+}
---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+	int rupee_count = 0;
+
+	public void AddRupees(int num_rupees)
+	{
+		rupee_count += num_rupees;
+	}
+
+	public int GetRupees()
+	{
+		return rupee_count;
+	}
+}
