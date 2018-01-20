a1d1bfb607f903d3572e875457f23cf32ea10f73
unknown <yuxuanye@umich.edu>
2018-01-20 15:12:53 -0500

    finished the pushable blocks, back to read more pages

--- Assets/pushable_wall.cs
+++ Assets/pushable_wall.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class pushable_wall : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Rigidbody rb;
+    GameObject playerGO;
+    ArrowKeyMovement arrowkeymovement;
+    private ArrowKeyMovement.Direction faceDirection;
+    private Vector3 originalPos;
+    private Vector3 checkVec;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        playerGO = GameObject.Find("Player");
+        originalPos = transform.position;
+    }
+
+    private void OnCollisionStay(Collision coll)
+    {
+        if (coll.collider.tag == "Player")
+        {
+            Vector3 direction = coll.transform.position - transform.position;
+            rb.AddForceAtPosition(direction.normalized, transform.position);
+            
+        }
+    }
+    private void Update()
+    {
+        checkVec = originalPos - transform.position;
+        if (Mathf.Abs(checkVec.x) >= 1)
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+            rb.position = new Vector3(originalPos.x - checkVec.x, originalPos.y, originalPos.z);
+            originalPos = rb.position;
+            return;
+        }
+        if (Mathf.Abs(checkVec.y) >= 1)
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+            rb.position = new Vector3(originalPos.x, originalPos.y - checkVec.y, originalPos.z);
+            originalPos = rb.position;
+            return;
+        }
+
+    }
+
 }
