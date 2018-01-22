19d86aa7a9b79c587bdaf4edc4051fce99056c55
Evan Czyzycki <eczy3826@gmail.com>
2018-01-22 12:40:22 -0500

    .

---
+++ Assets/Room.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+	GameObject[] active_members;
+
+	// Use this for initialization
+	void Start () {
+		foreach (GameObject obj in active_members)
+			obj.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
