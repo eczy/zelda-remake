cfb32218781b1ddb2a32faab8241f46e5e5028a0
Evan Czyzycki <eczy3826@gmail.com>
2018-01-22 14:22:28 -0500

    merged to my local repo

---
+++ Assets/WeaponManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour {
+
+	public GameObject weaponA = null;
+	public GameObject weaponB = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
